e97b8b1c088862658bb7c45f57ab37610b21d12c
ycmgg <110039709+ycmgg@users.noreply.github.com>
2022-10-19 17:40:40 +0800

    商品详情完成

--- subpkg/goods_detail/goods_detail.vue
+++ subpkg/goods_detail/goods_detail.vue
@@ -1,6 +1,38 @@
 <template>
-  <view>
-    
+  <view v-if="goods_info.goods_name" style="padding-bottom:50px;">
+    <!-- 轮播图 -->
+    <swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" :circular="true">
+      <swiper-item v-for="(val,i) in goods_info.pics" :key="i">
+        <image :src="val.pics_big" @click="preview(i)"></image>
+      </swiper-item>
+    </swiper>
+
+    <!-- 商品信息区域 -->
+    <view class="goods-info-box">
+      <!-- 商品价格 -->
+      <view class="price">￥{{goods_info.goods_price}}</view>
+      <!-- 信息主体区域 -->
+      <view class="goods-info-body">
+        <!-- 商品名称 -->
+        <view class="goods-name">{{goods_info.goods_name}}</view>
+        <!-- 收藏 -->
+        <view class="favi">
+          <uni-icons type="star" size="18" color="gray"></uni-icons>
+          <text>收藏</text>
+        </view>
+      </view>
+      <!-- 运费 -->
+      <view class="yf">快递：免运费</view>
+    </view>
+
+    <!-- 商品详情信息 -->
+    <rich-text :nodes="goods_info.goods_introduce"></rich-text>
+
+    <!-- 商品导航组件 -->
+    <view class="goods_nav">
+      <uni-goods-nav :fill="true" :options="options" :buttonGroup="buttonGroup" @click="onClick"
+        @buttonClick="buttonClick" />
+    </view>
   </view>
 </template>
 
@@ -8,12 +40,128 @@
   export default {
     data() {
       return {
-        
+        // 商品详情对象
+        goods_info: {},
+        options: [{
+          icon: 'shop',
+          text: '店铺',
+          infoBackgroundColor: '#007aff',
+          infoColor: "red"
+        }, {
+          icon: 'cart',
+          text: '购物车',
+          info: 2
+        }],
+        buttonGroup: [{
+            text: '加入购物车',
+            backgroundColor: '#ff0000',
+            color: '#fff'
+          },
+          {
+            text: '立即购买',
+            backgroundColor: '#ffa200',
+            color: '#fff'
+          }
+        ]
       };
+    },
+    onLoad(sss) {
+      const goods_id = sss.goods_id
+      this.getGoodsDetail(goods_id)
+    },
+    methods: {
+      async getGoodsDetail(goods_id) {
+        try {
+          const {
+            data: {
+              message
+            }
+          } = await uni.$http.get('/api/public/v1/goods/detail', {
+            goods_id
+          })
+          message.goods_introduce = message.goods_introduce.replace(/<img /g, '<img style="display: block;" ')
+            .replace(/webp/g, 'jpg')
+          this.goods_info = message
+        } catch (err) {
+          uni.$showMsg()
+        }
+      },
+      preview(i) {
+        uni.previewImage({
+          current: i,
+          urls: this.goods_info.pics.map(val => val.pics_big)
+        })
+      },
+      onClick(e) {
+        if(e.content.text === '购物车') {
+          uni.switchTab({
+            url: '/pages/cart/cart'
+          })
+        }
+      },
+      buttonClick(e) {
+        console.log(e)
+        this.options[2].info++
+      }
     }
   }
 </script>
 
 <style lang="scss">
+  swiper {
+    height: 750rpx;
 
+    image {
+      height: 100%;
+      width: 100%;
+    }
+  }
+
+  // 商品信息区域的样式
+  .goods-info-box {
+    padding: 10px;
+    padding-right: 0;
+
+    .price {
+      color: #c00000;
+      font-size: 18px;
+      margin: 10px 0;
+    }
+
+    .goods-info-body {
+      display: flex;
+      justify-content: space-between;
+
+      .goods-name {
+        font-size: 13px;
+        padding-right: 10px;
+      }
+
+      // 收藏区域
+      .favi {
+        width: 120px;
+        font-size: 12px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        border-left: 1px solid #efefef;
+        color: gray;
+      }
+    }
+
+    // 运费
+    .yf {
+      margin: 10px 0;
+      font-size: 12px;
+      color: gray;
+    }
+  }
+
+  .goods_nav {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+  }
 </style>
