7b4349fd1706dc9c680a490d0e5be456220864a0
ycmgg <110039709+ycmgg@users.noreply.github.com>
2022-10-19 16:26:20 +0800

    完成了商品列表页面的开发

---
+++ components/my-goods/my-goods.vue
@@ -0,0 +1,67 @@
+<template>
+  <view class="goods-itme">
+    <view class="goods-itme-left">
+      <image :src="val.goods_small_logo || defaultPic"></image>
+    </view>
+    <view class="goods-itme-right">
+      <text class="name">{{val.goods_name}}</text>
+      <text class="price">$ {{val.goods_price | tofixed}}</text>
+    </view>
+  </view>
+</template>
+
+<script>
+  export default {
+    name: "my-goods",
+    props: {
+      val: {
+        type: Object,
+        default: {}
+      }
+    },
+    filters: {
+      tofixed(unm) {
+        return Number(unm).toFixed(2)
+      }
+    },
+    data() {
+      return {
+        // 默认的空图片
+        defaultPic: 'https://img3.doubanio.com/f/movie/8dd0c794499fe925ae2ae89ee30cd225750457b4/pics/movie/celebrity-default-medium.png'
+      };
+    }
+  }
+</script>
+
+<style lang="scss">
+  .goods-itme {
+    display: flex;
+    padding: 10px 5px;
+    border-bottom: 1px solid #8effee;
+
+    .goods-itme-left {
+      margin-right: 5px;
+
+      image {
+        width: 100px;
+        height: 100px;
+        display: block;
+      }
+    }
+
+    .goods-itme-right {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+
+      .name {
+        font-size: 13px;
+      }
+
+      .price {
+        font-size: 16px;
+        color: #C00000;
+      }
+    }
+  }
+</style>
--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,6 +1,8 @@
 <template>
-  <view>
-    
+  <view class="goods-list">
+    <view v-for="(val,i) in goodsList" :key="i" @click="gotoDoodsDetail(val.goods_id)">
+      <my-goods :val="val" />
+    </view>
   </view>
 </template>
 
@@ -8,8 +10,58 @@
   export default {
     data() {
       return {
-        
+        queryObj: {
+          query: '',
+          cid: '',
+          pagenum: 1,
+          pagesize: 10
+        },
+        goodsList: [],
+        total: 0,
+        isloading: false
       };
+    },
+    onLoad(op) {
+      this.queryObj.query = op.query || ''
+      this.queryObj.cid = op.cid || ''
+      this.getGoodsList()
+    },
+    methods: {
+      async getGoodsList(yc) {
+        try {
+          this.isloading = true
+          const {
+            data: {
+              message
+            }
+          } = await uni.$http.get('/api/public/v1/goods/search', this.queryObj)
+          this.total = message.total
+          this.goodsList = [...this.goodsList,...message.goods]
+          this.isloading = false
+          yc && yc()
+        } catch (err) {
+          uni.$showMsg()
+        }
+      },
+      gotoDoodsDetail (id) {
+        uni.navigateTo({
+          url: '/subpkg/goods_detail/goods_detail?goods_id=' + id
+        })
+      }
+    },
+    onReachBottom() {
+        if(this.isloading) return
+        if(this.goodsList.length >= this.total) return uni.$showMsg('没有更多数据了!')
+        this.queryObj.pagenum++
+        this.getGoodsList()
+    },
+    onPullDownRefresh () {
+      this.queryObj.pagenum = 1
+      this.total = 0
+      this.isloading = false 
+      this.goodsList = []
+      
+      this.getGoodsList(() => uni.stopPullDownRefresh())
     }
   }
 </script>
